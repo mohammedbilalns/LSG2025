540a7b0598c3c9e5571a8ca7bc09090ba561d858
Jinoytommanjaly <jinoytommanjaly@gmail.com>
2025-12-10 19:46:18 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/kerala-election-portal/', // Set base URL for GitHub Pages
+  base: '/KeralaLSGElection/', // Set base URL for GitHub Pages
 })
